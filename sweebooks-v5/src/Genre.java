16cb0640c6bdff27a6f3661b4a287d824037f15c
anthonyw29 <63148189+anthonyw29@users.noreply.github.com>
2020-08-10 22:13:09 +0700

    template class

--- sweebooks-v5/src/Genre.java
+++ sweebooks-v5/src/Genre.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Genre {
 
+	private String id;
+	private String type;
+	
 	public Genre() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public List<Genre> all(){
+		return new ArrayList<Genre>();
+	}
+	
+	public Genre insert() {
+		return new Genre();
+	}
+	
+	public Genre getByType(String type) {
+		return new Genre();
 	}
 
 }
