0ed01122f9938fd9996132059714b21e49616ac2
agvrn <63383886+agvrn@users.noreply.github.com>
2020-08-10 22:19:36 +0700

    template class rian

--- sweebooks-v5/src/Borrow.java
+++ sweebooks-v5/src/Borrow.java
@@ -1,8 +1,44 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Borrow {
+	
+	private String id;
+	private String memberId;
+	private String status;
+	private String borrowTimestamp;
 
 	public Borrow() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Borrow find(String id) {
+		return new Borrow();
+	}
+	
+	public Borrow insert() {
+		return new Borrow();
+	}
+	
+	public Borrow update() {
+		return new Borrow();
+	}
+	
+	public boolean isBookStillBorrowing(String userId, String bookId) {
+		return true;
+	}
+	
+	public int getCountBookStillBorrowing(String userId) {
+		return 0;
+	}
+	
+	public List<Borrow> getPendingStatus(boolean isOnlyCurrentMember){
+		return new ArrayList<Borrow>();
+	}
+	
+	public List<Borrow> getAcceptStatus(Date date, boolean isOnlyCurrentMember){
+		return new ArrayList<Borrow>();
 	}
 
 }
