3d3279a1abef18e7915506f00964d8db112adc7e
agvrn <63383886+agvrn@users.noreply.github.com>
2020-08-10 20:56:20 +0700

    Create BorrowTransactionHandler.java

---
+++ sweebooks-v5/src/BorrowTransactionHandler.java
@@ -0,0 +1,14 @@
+import javax.swing.JInternalFrame;
+
+public class BorrowTransactionHandler {
+
+	public JInternalFrame showBorrowForm() {
+		JInternalFrame JIF = new JInternalFrame();
+		return JIF;
+	}
+	
+	public BorrowTransactionHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
