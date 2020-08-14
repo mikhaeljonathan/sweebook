43fed5b35b57022af71b2b21f395df1b071ecf1d
agvrn <63383886+agvrn@users.noreply.github.com>
2020-08-14 22:56:46 +0700

    login&createForm

--- sweebooks-v5/src/main/LoginForm.java
+++ sweebooks-v5/src/main/LoginForm.java
@@ -1,22 +1,80 @@
 package main;
 
+import javax.swing.JFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 import controller.MemberHandler;
 import model.Member;
+import view.CreateMembershipForm;
 
-public class LoginForm {
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+
+public class LoginForm extends JFrame{
+	private JTextField textField_Username;
+	private JPasswordField passwordField;
 
 	public LoginForm() {
 		
-		// TODO: after the create membership button pressed, then do below
+		//Create UI
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 500);
 		
-		// Show the form
-		MemberHandler mh = new MemberHandler();
-		mh.showCreateMembershipForm();
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
 		
-		// Handle the input field
-		handleCreateMembershipInput(mh);
+		textField_Username = new JTextField();
+		textField_Username.setBounds(123, 94, 248, 20);
+		getContentPane().add(textField_Username);
+		textField_Username.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(49, 97, 64, 14);
+		getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(49, 128, 64, 14);
+		getContentPane().add(lblPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(119, 172, 181, 23);
+		getContentPane().add(btnLogin);
+		
+		JButton btnCreate = new JButton("Create Membership");
+		btnCreate.setBounds(119, 209, 181, 23);
+		getContentPane().add(btnCreate);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(123, 125, 248, 20);
+		getContentPane().add(passwordField);
+		
+		btnCreate.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new CreateMembershipForm();
+			}
+		});
+		
+		setVisible(true);
+		
+		
+//		// TODO: after the create membership button pressed, then do below
+//		
+//		// Show the form
+//		MemberHandler mh = new MemberHandler();
+//		mh.showCreateMembershipForm();
+//		
+//		// Handle the input field
+//		handleCreateMembershipInput(mh);
 		
 	}
 	
@@ -51,5 +109,4 @@ public class LoginForm {
 		}
 		
 	}
-	
 }
--- sweebooks-v5/src/view/CreateMembershipForm.java
+++ sweebooks-v5/src/view/CreateMembershipForm.java
@@ -4,8 +4,28 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import controller.MemberHandler;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JToggleButton;
+import javax.swing.JEditorPane;
+import javax.swing.JSlider;
+import javax.swing.JScrollBar;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
 
 public class CreateMembershipForm extends JFrame{
+	private JTextField textField_Name;
+	private JTextField textField_Username;
+	private JTextField textField_Password;
 	
 	public CreateMembershipForm() {
 		
@@ -15,9 +35,59 @@ public class CreateMembershipForm extends JFrame{
 		setSize(500, 500);
 		
 		setLocationRelativeTo(null);
-		JPanel panel1 = new JPanel();
-		add(panel1);
+		getContentPane().setLayout(null);
+		
+		textField_Name = new JTextField();
+		textField_Name.setBounds(111, 57, 242, 20);
+		getContentPane().add(textField_Name);
+		textField_Name.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(29, 57, 49, 20);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblGender = new JLabel("Gender");
+		lblGender.setBounds(29, 88, 49, 20);
+		getContentPane().add(lblGender);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Male", "Female"}));
+		comboBox.setBounds(111, 88, 242, 20);
+		getContentPane().add(comboBox);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setBounds(29, 119, 49, 20);
+		getContentPane().add(lblAddress);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(29, 181, 49, 20);
+		getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(29, 212, 49, 20);
+		getContentPane().add(lblPassword);
+		
+		JButton btnCreate = new JButton("Create Member");
+		btnCreate.setBounds(140, 292, 176, 23);
+		getContentPane().add(btnCreate);
+		
+		textField_Username = new JTextField();
+		textField_Username.setColumns(10);
+		textField_Username.setBounds(111, 181, 242, 20);
+		getContentPane().add(textField_Username);
+		
+		textField_Password = new JTextField();
+		textField_Password.setColumns(10);
+		textField_Password.setBounds(111, 212, 242, 20);
+		getContentPane().add(textField_Password);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(111, 119, 242, 51);
+		getContentPane().add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
 		setVisible(true);
 	}
-	
 }
