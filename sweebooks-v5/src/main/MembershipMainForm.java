262bbc4ba91639469ce80adb109f0c07af11f3d8
anthonyw29 <63148189+anthonyw29@users.noreply.github.com>
2020-08-18 10:54:59 +0700

    tes

--- sweebooks-v5/src/main/MembershipMainForm.java
+++ sweebooks-v5/src/main/MembershipMainForm.java
@@ -1,95 +1,88 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import controller.BookHandler;
 import controller.BorrowBookHandler;
-import controller.MemberHandler;
-import model.Book;
-import view.BorrowBookForm;
-import view.ViewBorrowForm;
-import javax.swing.*;
-import java.awt.*;
+import controller.BorrowTransactionHandler;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class MembershipMainForm extends JFrame{
- 
-	 public MembershipMainForm() {
-		 setTitle("Membership Main Form");
-		 getContentPane().setLayout(null);
-		 setSize(900, 500);
-		 setLocationRelativeTo(null);
-	  
-		 JButton btnBorrowBook = new JButton("Borrow Book");
-		 btnBorrowBook.addMouseListener(new MouseAdapter() {
-		 @Override
-		  public void mouseClicked(MouseEvent e) {
-			 BorrowBookForm borrowForm = new BorrowBookForm();
-		     borrowForm.setResizable(false);
-//   		 borrowForm.setBounds(174, 11, 500, 261);
-		     borrowForm.setLocation(174, 11);
-		     getContentPane().add(borrowForm);
-		     borrowForm.getContentPane().setLayout(null);
-		     borrowForm.setVisible(true);
-		  }
-		 });
+
+	private static final long serialVersionUID = 1L;
+
+	public MembershipMainForm() {
+		
+		setTitle("Membership Main Form");
+		getContentPane().setLayout(null);
+		setSize(900, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		// Borrow Book Button
+		JButton borrowBookBtn = new JButton("Borrow Book");
+		borrowBookBtn.setBounds(10, 11, 140, 35);
+		add(borrowBookBtn);
+		borrowBookBtn.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				add(new BorrowBookHandler().showBorrowBookForm());
+			    
+			 }
+			
+		});
+		
+		// View Book Button
+		JButton bookViewBtn = new JButton("Book View");
+		bookViewBtn.setBounds(10, 79, 140, 35);
+		add(bookViewBtn);
+		bookViewBtn.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				add(new BookHandler().showViewBookForm());
+				
+			}
+
+		});
+		
+		// View Borrow Button
+		JButton borrowViewBtn = new JButton("Borrow View");
+		borrowViewBtn.setBounds(10, 148, 140, 35);
+		add(borrowViewBtn);
+		borrowViewBtn.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				add(new BorrowTransactionHandler().showBorrowForm());
+				
+			}
+			
+		});
 		 
-		 btnBorrowBook.setBounds(10, 11, 140, 35);
-		 getContentPane().add(btnBorrowBook);
-		  
-		 JButton btnBookView = new JButton("Book View");
-		 btnBookView.addMouseListener(new MouseAdapter() {
-		 @Override
-		  public void mouseClicked(MouseEvent e) {
-		    
-		   }
-		  });
-		 btnBookView.setBounds(10, 79, 140, 35);
-		 getContentPane().add(btnBookView);
-		  
-		 JButton btnBorrowView = new JButton("Borrow View");
-		 btnBorrowView.setBounds(10, 148, 140, 35);
-		 getContentPane().add(btnBorrowView);
-		  
-		 JButton btnBorrowHistory = new JButton("Borrow History");
-		 btnBorrowHistory.setBounds(10, 215, 140, 35);
-		 getContentPane().add(btnBorrowHistory);
-		  
-		 // TODO: begitu membership klik borrow book menu langsung execute ini:
-		 BorrowBookHandler bbh = new BorrowBookHandler();
-		 bbh.showBorrowBookForm();
-		  
-		 // Get list all of the available books to be borrowed
-		 List<Book> listOfAvailableBooks = new ArrayList<Book>();
-		 listOfAvailableBooks = bbh.getAvailableBook();
-		 // TODO: tampilin semua List<Book> nya
-		  
-		 // TODO: kalau tombol cart diteken execute ini:
-		  
-		 // Get list all of the books in the cart
-		 List<Book> cartList = new ArrayList<Book>();
-		 cartList = bbh.getCart();
-		 // TODO: tampilin semua List<Book> nya
-		  
-		 // TODO: kalau salah satu buku di listOfAvailableBooks diteken keluar tombol add to cart dan kalau diteken execute ini
-		 Book b = new Book();
-		  
-		 if (bbh.addToCart(b)) {
-		  // TODO: keluarin success message
-		 } else {
-		  // TODO: keluarin error message
-		 }
-		  
-		  
-		 // TODO: kalau cartnya keluar, bakal mucnul juga borrow button, terus kalo diklik execute this:
-		 if (bbh.borrowBook()) {
-		  // TODO: keluarin success message
-		  
-		 } else {
-		  // TODO: keluarin error message
-		 }
-		  
-		 setVisible(true);
+		// Borrow History Button
+		JButton borrowHistoryBtn = new JButton("Borrow History");
+		borrowHistoryBtn.setBounds(10, 215, 140, 35);
+		add(borrowHistoryBtn);
+		borrowHistoryBtn.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				add(new BorrowTransactionHandler().showBorrowHistoryForm());
+				
+			}
+			
+		});
+		
 	 }
+	
 }
--- sweebooks-v5/src/view/BorrowBookForm.java
+++ sweebooks-v5/src/view/BorrowBookForm.java
@@ -1,7 +1,7 @@
 package view;
 
 import java.awt.Color;
-
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,7 +10,6 @@ import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -22,39 +21,93 @@ import model.Book;
 
 public class BorrowBookForm extends JInternalFrame{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private BorrowBookHandler bbh = new BorrowBookHandler();
 	private Book b = new Book();
 	
-	public JPanel panelBookInCartInfo(String BookName, String Genre, String ISBN, int Quantity) {
-		JPanel panelBookInCartInfo = new JPanel();
-		panelBookInCartInfo.setSize(219, 50);
-		panelBookInCartInfo.setLayout(new GridLayout(3, 2, 0, 1));
-		panelBookInCartInfo.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+	public BorrowBookForm() {
+		setResizable(false);
+		setTitle("Borrow Book Form");
+		setSize(800, 400); //ukuran gui
+		setLocation(160, 10);
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //biar kalo di close ga running di background
+		getContentPane().setLayout(new GridLayout(1, 2, 0, 0));
+		setVisible(true);
 		
-		JLabel lblName = new JLabel(BookName);
-		panelBookInCartInfo.add(lblName);
+		List<Book> listOfAvailableBooks = new ArrayList<Book>();
+		listOfAvailableBooks = bbh.getAvailableBook();
 		
-		JButton btnRemoveFromCart = new JButton("Remove");
-		panelBookInCartInfo.add(btnRemoveFromCart);
+		// TODO: create UI
+		//-------------- Bagian List Book -----------------------------
+		JPanel listBookPanel = new JPanel();
+		listBookPanel.setLayout(new GridLayout(20, 0, 0, 10));
+		JScrollPane listBookScrollPane = new JScrollPane(listBookPanel);
+		getContentPane().add(listBookScrollPane);
+		//-------------------------------------------------------------
 		
-		JLabel lblGenre = new JLabel(Genre);
-		panelBookInCartInfo.add(lblGenre);
+		//-------------- Bagian Panel Cart -----------------------
+		JPanel listBookInCartPanel = new JPanel();
+		getContentPane().add(listBookInCartPanel);
+		//-------------------------------------------------
 		
-		JLabel lblEmpty = new JLabel("");
-		panelBookInCartInfo.add(lblEmpty);
+//		BorrowBookHandler bbh = new BorrowBookHandler();
+		// Get list all of the available books to be borrowed
+//		List<Book> listOfAvailableBooks = new ArrayList<Book>();
+//		listOfAvailableBooks = bbh.getAvailableBook();
 		
-		JLabel lblISBN = new JLabel(ISBN);
-		panelBookInCartInfo.add(lblISBN);
+		// TODO: tampilin semua List<Book> nya
+		// public JPanel panelBookListInfo( STRING, STRING, STRING, INT)
+		for (Book books : listOfAvailableBooks) {
+			listBookPanel.add(ListBookInfo(listBookPanel.getWidth(),books.getName(), books.getGenreId(), books.getIsbn(), books.getQuantity()));
+		}
 		
-		JLabel lblQuantity = new JLabel(String.valueOf(Quantity));
-		panelBookInCartInfo.add(lblQuantity);
+		// TODO: kalau tombol cart diteken execute ini:
+//		btnCart.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				if (!panelMainCart.isVisible()) { 
+//					panelMainCart.setVisible(true); //kalau ga nyala, nyalain
+//				} else {
+//					panelMainCart.setVisible(false); //kalau nyala, matiin
+//				}
+//			}
+//		});
 		
-		return panelBookInCartInfo;
+		// Get list all of the books in the cart
+		List<Book> cartList = new ArrayList<Book>();
+		cartList = bbh.getCart();
+		// TODO: tampilin semua List<Book> nya dari cart
+		// public JPanel panelBookInCartInfo( STRING, STRING, STRING, INT)
+//		int iCartList = 0;
+//		for (Book books : cartList) {
+//			iCartList++;
+//			panelCart.add(panelBookInCartInfo(books.getName(), books.getGenreId(), books.getIsbn(), books.getQuantity()));
+//			panelCart.setLayout(new GridLayout(1+iCartList,0,0,10));
+//		}
+		
+		// TODO: kalau salah satu buku di listOfAvailableBooks diteken keluar tombol add to cart dan kalau diteken execute ini
+//		Book b = new Book();
+		
+		// TODO: kalau cartnya keluar, bakal mucnul juga borrow button, terus kalo diklik execute this:
+		if (bbh.borrowBook()) {
+			// TODO: keluarin success message
+			
+		} else {
+			// TODO: keluarin error message
+		}
+				
+		
+		// TODO: kalau salah satu item cart di klik, bakal muncul tombol remove cart
+		// then keluarin confirmation dialog, klao yes then execute this:
+		
 	}
 	
-	public JPanel panelBookListInfo(String BookName, String Genre, String ISBN, int Quantity) {
+	
+	public JPanel ListBookInfo(int width, String BookName, String Genre, String ISBN, int Quantity) {
 		JPanel panelBookListInfo = new JPanel();
-		panelBookListInfo.setSize(219, 50);
 		panelBookListInfo.setLayout(new GridLayout(3, 2, 0, 1));
 		panelBookListInfo.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 		
@@ -91,112 +144,43 @@ public class BorrowBookForm extends JInternalFrame{
 		return panelBookListInfo;
 	}
 	
-	public BorrowBookForm() {
+	
+	public JPanel panelBookInCartInfo(String BookName, String Genre, String ISBN, int Quantity) {
+		JPanel panelBookInCartInfo = new JPanel();
+		panelBookInCartInfo.setSize(219, 50);
+		panelBookInCartInfo.setLayout(new GridLayout(3, 2, 0, 1));
+		panelBookInCartInfo.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 		
-		// TODO: create UI
-		setSize(600, 450);
-		setTitle("View Borrow Form");
-		setBorder(null);
-		getContentPane().setLayout(null);
+		JLabel lblName = new JLabel(BookName);
+		panelBookInCartInfo.add(lblName);
 		
-		//-------------- Bagian List Book -----------------------------
-		JPanel panelMainListBook = new JPanel();
-		panelMainListBook.setBounds(10, 11, 297, 401);
-		getContentPane().add(panelMainListBook);
-		panelMainListBook.setLayout(null);
-		
-		//panel buat list book
-		JPanel panelListBook = new JPanel();
-		panelListBook.setBounds(10, 11, 277, 379);
-		JScrollPane scrollPaneListBook = new JScrollPane(panelListBook);
-		scrollPaneListBook.setBounds(10, 11, 277, 379);
-		scrollPaneListBook.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		panelMainListBook.add(scrollPaneListBook);
-		//-------------------------------------------------------------
-		
-		//-------------- Bagian Panel Cart -----------------------
-		JButton btnCart = new JButton("Cart");
-		btnCart.setBounds(412, 11, 89, 23);
-		getContentPane().add(btnCart);
-		
-		// panel main cart
-		JPanel panelMainCart = new JPanel();
-		panelMainCart.setBounds(317, 38, 273, 374);
-		getContentPane().add(panelMainCart);
-		panelMainCart.setLayout(null);
-		
-		//panel list di dalam cart
-		JPanel panelCart = new JPanel();
-		panelCart.setBounds(271, 54, 219, 261);
-		JScrollPane scrollPaneCart = new JScrollPane(panelCart); //scroll buat panel
-		scrollPaneCart.setBounds(10, 11, 253, 318);
-		scrollPaneCart.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		panelMainCart.add(scrollPaneCart);
-		
-		//button borrow
-		JButton btnBorrow = new JButton("Borrow");
-		btnBorrow.setBounds(98, 340, 89, 23);
-		panelMainCart.add(btnBorrow);
-		
-		panelMainCart.setVisible(false);
-		//-------------------------------------------------
-		
-//		BorrowBookHandler bbh = new BorrowBookHandler();
-		// Get list all of the available books to be borrowed
-		List<Book> listOfAvailableBooks = new ArrayList<Book>();
-		listOfAvailableBooks = bbh.getAvailableBook();
-		// TODO: tampilin semua List<Book> nya
-		// public JPanel panelBookListInfo( STRING, STRING, STRING, INT)
-		int iBookList = 0;
-		for (Book books : listOfAvailableBooks) {
-			iBookList++;
-			panelListBook.add(panelBookListInfo(books.getName(), books.getGenreId(), books.getIsbn(), books.getQuantity()));
-			panelListBook.setLayout(new GridLayout(1+iBookList,0,0,10));
-		}
-		
-		// TODO: kalau tombol cart diteken execute ini:
-		btnCart.addMouseListener(new MouseAdapter() {
+		JButton btnRemoveFromCart = new JButton("Remove");
+		panelBookInCartInfo.add(btnRemoveFromCart);
+		btnRemoveFromCart.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (!panelMainCart.isVisible()) { 
-					panelMainCart.setVisible(true); //kalau ga nyala, nyalain
-				} else {
-					panelMainCart.setVisible(false); //kalau nyala, matiin
+				if (bbh.removeCart(b)) {
+					// TODO: keluarin success message
+					JOptionPane.showMessageDialog(null, "Remove book success!");
+				} else if (!bbh.removeCart(b)) {
+					// TODO: keluarin error message
+					JOptionPane.showMessageDialog(null, "Failed removing book!");
 				}
 			}
 		});
 		
-		// Get list all of the books in the cart
-		List<Book> cartList = new ArrayList<Book>();
-		cartList = bbh.getCart();
-		// TODO: tampilin semua List<Book> nya dari cart
-		// public JPanel panelBookInCartInfo( STRING, STRING, STRING, INT)
-		int iCartList = 0;
-		for (Book books : cartList) {
-			iCartList++;
-			panelCart.add(panelBookInCartInfo(books.getName(), books.getGenreId(), books.getIsbn(), books.getQuantity()));
-			panelCart.setLayout(new GridLayout(1+iCartList,0,0,10));
-		}
+		JLabel lblGenre = new JLabel(Genre);
+		panelBookInCartInfo.add(lblGenre);
 		
-		// TODO: kalau salah satu buku di listOfAvailableBooks diteken keluar tombol add to cart dan kalau diteken execute ini
-//		Book b = new Book();
+		JLabel lblEmpty = new JLabel("");
+		panelBookInCartInfo.add(lblEmpty);
 		
-		// TODO: kalau cartnya keluar, bakal mucnul juga borrow button, terus kalo diklik execute this:
-		if (bbh.borrowBook()) {
-			// TODO: keluarin success message
-			
-		} else {
-			// TODO: keluarin error message
-		}
-				
+		JLabel lblISBN = new JLabel(ISBN);
+		panelBookInCartInfo.add(lblISBN);
 		
-		// TODO: kalau salah satu item cart di klik, bakal muncul tombol remove cart
-		// then keluarin confirmation dialog, klao yes then execute this:
-		if (bbh.removeCart(b)) {
-			// TODO: keluarin success message
-		} else {
-			// TODO: keluarin error message
-		}
+		JLabel lblQuantity = new JLabel(String.valueOf(Quantity));
+		panelBookInCartInfo.add(lblQuantity);
 		
+		return panelBookInCartInfo;
 	}
 }
